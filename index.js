675496fbe885db8a4737a2346c444bf220442972
Siuspras <50543989+Siuspras@users.noreply.github.com>
2019-05-15 20:07:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,146 +6,156 @@ bot.on('ready', () =>{
    console.log(' this bot is online ! ');
 })
 bot.on('message',  message => {
-   if(message.content === 'ping')
-   message.channel.send('pong!')
-   
-   if(message.content === 'C test'){
+   if (receivedfullCommand.startsWith("!")) {
+      processCommand(command)
+  }
+
+})
+
+function processCommand(command){
+   let fullCommand = command.content.substr(1)
+   let splitCommand = FullCommand.split(" ")
+   let primaryCommand = splitCommand[0]
+   let arguments = splitCommand.slice(1)
+
+   console.log("Command Received "+ primaryCommand)
+   console.log("Arguments : "+arguments)
+
+   if(fullCommand === 'c test'){
       message.channel.send('yes this is a test on test ' + message.author)
    }
    
-   if(message.content === 'addalex'){
+   if(fullCommand === 'c addalex'){
       message.channel.send(message.author + ', you will be add to Alexiel Role')
       let memberRole = message.member.guild.roles.find('name','Alexiel')
       message.member.addRole(memberRole)
    }
-   if(message.content === 'remalex'){
+   if(fullCommand === 'c remalex'){
       message.channel.send(message.author + ', you will be remove from Alexiel Role')
       let memberRole2 = message.member.guild.roles.find('name','Alexiel')
       message.member.removeRole(memberRole2)
    }
-   if(message.content === 'addgrim'){
+   if(fullCommand === 'c addgrim'){
       message.channel.send(message.author + ', you will be add to Grimnir Role')
       let memberRole3 = message.member.guild.roles.find('name','Grimnir')
       message.member.addRole(memberRole3)
    }
-   if(message.content === 'remgrim'){
+   if(fullCommand === 'c remgrim'){
       message.channel.send(message.author + ', you will be remove from Grimnir Role')
       let memberRole3 = message.member.guild.roles.find('name','Grimnir')
       message.member.removeRole(memberRole3)
    }
    
-   if(message.content === 'addeur'){
+   if(fullCommand === 'c addeur'){
       message.channel.send(message.author + ', you will be add to Europe Role')
       let memberRole4 = message.member.guild.roles.find('name','Europe')
       message.member.addRole(memberRole4)
    }
-   if(message.content === 'remeur'){
+   if(fullCommand === 'c remeur'){
       message.channel.send(message.author + ', you will be remove from Europe Role')
       let memberRole5 = message.member.guild.roles.find('name','Europe')
       message.member.removeRole(memberRole5)
    }
-   if(message.content === 'addshiva'){
+   if(fullCommand === 'c addshiva'){
       message.channel.send(message.author + ', you will be add to Shiva Role')
       let memberRole6 = message.member.guild.roles.find('name','Shiva')
       message.member.addRole(memberRole6)
    }
-   if(message.content === 'remshiva'){
+   if(fullCommand === 'c remshiva'){
       message.channel.send(message.author + ', you will be remove from Shiva Role')
       let memberRole7 = message.member.guild.roles.find('name','Shiva')
       message.member.removeRole(memberRole7)
    }
-   if(message.content === 'addmeta'){
+   if(fullCommand === 'c addmeta'){
       message.channel.send(message.author + ', you will be add to Metatron Role')
       let memberRole8 = message.member.guild.roles.find('name','Metatron')
       message.member.addRole(memberRole8)
    }
-   if(message.content === 'remmeta'){
+   if(fullCommand === 'c remmeta'){
       message.channel.send(message.author + ', you will be remove from Metatron Role')
       let memberRole9 = message.member.guild.roles.find('name','Metatron')
       message.member.removeRole(memberRole9)
    }
-   if(message.content === 'addava'){
+   if(fullCommand === 'c addava'){
       message.channel.send(message.author + ', you will be add to Avatar Role')
       let memberRole10 = message.member.guild.roles.find('name','Avatar')
       message.member.addRole(memberRole10)
    }
-   if(message.content === 'remava'){
+   if(fullCommand === 'c remava'){
       message.channel.send(message.author + ', you will be remove from Avatar Role')
       let memberRole11 = message.member.guild.roles.find('name','Avatar')
       message.member.removeRole(memberRole11)
    }
-    if(message.content === 'addubaha'){
+    if(fullCommand === 'c addubaha'){
       message.channel.send(message.author + ', you will be add to Ultimate Bahamut Role')
       let memberRole12 = message.member.guild.roles.find('name','Ubaha')
       message.member.addRole(memberRole12)
    }
-   if(message.content === 'remubaha'){
+   if(fullCommand === 'c remubaha'){
       message.channel.send(message.author + ', you will be remove from Ultimate Bahamut Role')
       let memberRole13 = message.member.guild.roles.find('name','Ubaha')
       message.member.removeRole(memberRole13)
    }
-   if(message.content === 'addubahaHL'){
+   if(fullCommand === 'c addubahaHL'){
       message.channel.send(message.author + ', you will be add to Ubaha HL Role')
       let memberRole12 = message.member.guild.roles.find('name','UbahaHl')
       message.member.addRole(memberRole12)
    }
-   if(message.content === 'remubahaHL'){
+   if(fullCommand === 'c remubahaHL'){
       message.channel.send(message.author + ', you will be remove from UBaha HL Role')
       let memberRole13 = message.member.guild.roles.find('name','UbahaHl')
       message.member.removeRole(memberRole13)
    }
-   if(message.content === 'C ColoGrid'){
+   if(fullCommand === 'c ColoGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/47075')
    }
-   if(message.content === 'C LeviGrid'){
+   if(fullCommand === 'c LeviGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/47186')
    }
-   if(message.content === 'C YggGrid'){
+   if(fullCommand === 'c YggGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/47428')
    }
-   if(message.content === 'C TiamatGrid'){
+   if(fullCommand === 'c TiamatGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/47217')
    }
-   if(message.content === 'C CelesGrid'){
+   if(fullCommand === 'c CelesGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/47695')
    }
-   if(message.content === 'C ChevGrid'){
+   if(fullCommand === 'c ChevGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/47571')
    }
-   if(message.content === 'C AgniGrid'){
+   if(fullCommand === 'c AgniGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/21595')
    }
-   if(message.content === 'C VarunaGrid'){
+   if(fullCommand === 'c VarunaGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/21615')
    }
-   if(message.content === 'C TitanGrid'){
+   if(fullCommand === 'c TitanGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/21593')
    }
-   if(message.content === 'C ZephyGrid'){
+   if(fullCommand === 'c ZephyGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/21597')
    }
-   if(message.content === 'C HadesGrid'){
+   if(fullCommand === 'c HadesGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/21604')
    }
-   if(message.content === 'C ZeusGrid'){
+   if(fullCommand === 'c ZeusGrid'){
       message.channel.send(message.author +', here is the link for your reference')
       message.channel.send('グランブルーファンタジー.gamewith.jp/article/show/21596')
    }
 
-
-
-   if(message.content === 'C help'){
+   if(fullCommand === 'c help'){
       message.channel.send(message.author + ' so far there is ')
       message.channel.send('1 - C help for all list of command\n' +
       '2 - C ColoGrid for Magna Colosus Grid, C AgniGrid for Primal Agni Grid\n'+
@@ -164,6 +174,10 @@ bot.on('message',  message => {
       '15- addUbaha (add role UltimateBahamut)  / remUbaha (remove role UltimateBahamut)\n'+
       '16- addubahaHL (add role Ubahamut Hl) / remubahaHL (remove role Ubahamut HL)');
    }
-})
+
+   else {
+      receivedMessage.channel.send("I don't undestand the command. try '!c help' for more commands")
+   }
+}
 
 bot.login(token);
